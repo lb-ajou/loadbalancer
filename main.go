207087bf4cd006e7f398f540bfb319dc765ff6d0
천 진강 <jjjjjk12@ajou.ac.kr>
2026-06-04 20:08:35 +0900

    chore(repo): 기본 실행 진입점 추가

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"reverseproxy-poc/internal/cli"
+)
+
+func main() {
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	err := cli.Run(ctx, cli.Options{
+		Args:   os.Args[1:],
+		Stdout: os.Stdout,
+		Stderr: os.Stderr,
+	})
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
